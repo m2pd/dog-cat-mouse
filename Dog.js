1136c464c1ca6a6466edeb766a7e145159c24807
unknown <vuatrochoi201@gmail.com>
2019-03-30 16:27:10 +0700

    say Hi methob

--- Dog.js
+++ Dog.js
@@ -1,3 +1,4 @@
+var chalk = require('chalk')
 function Dog(name){
 	this.name = name;
 	this.stomash =[];
@@ -6,7 +7,7 @@ Dog.prototype.eat = function(cat){
 	this.stomash.push(cat);
 };
 Dog.prototype.sayHi = function(){
-	console.log('Hi !im Dog' + chalk.red(this.name));
+	console.log('Hi ,im Dog. My name is ' + chalk.red(this.name));
 
 };
-module.exports = Dog;
+module.exports = Dog; 
