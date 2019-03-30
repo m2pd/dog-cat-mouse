60d131ede1b68fb141c0a66f3cfb4c26aa6141c5
unknown <vuatrochoi201@gmail.com>
2019-03-30 16:20:31 +0700

    add dog function name Tom

--- Dog.js
+++ Dog.js
@@ -1,6 +1,12 @@
-function Dog(){
+function Dog(name){
+	this.name = name;
 	this.stomash =[];
 }
 Dog.prototype.eat = function(cat){
 	this.stomash.push(cat);
-}
+};
+Dog.prototype.sayHi = function(){
+	console.log('Hi !im Dog' + chalk.red(this.name));
+
+};
+module.exports = Dog;
